bdd17799e682d7b1c8bcf980a314746ca562b20f
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-12 13:56:15 +0100

    angular reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div><app-customer></app-customer></div>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
