581d8dbc8abcd3771fd1f88df43f1d30e9f9ee53
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-12 21:07:44 +0100

    created a template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CustomerComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
+  customer = new Customer();
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // this method is executed when a user submits the form. we pass in the form model(customerForm: NgForm) from the template so we can check the form state and view its values.
+  save(customerForm: NgForm): void {
+    // we call a service here to save our data to a back-end server
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
 }
