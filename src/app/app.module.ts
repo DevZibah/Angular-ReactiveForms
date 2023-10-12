f5cb5a3bffe1d38eb875f5423419d8e60f16b883
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-12 23:03:44 +0100

    created angular reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CustomerOneComponent } from './customers/customer-one.component';
 
 @NgModule({
   declarations: [AppComponent, CustomerComponent, CustomerOneComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/customers/customer-one.component.ts
+++ src/app/customers/customer-one.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer-one',
   templateUrl: './customer-one.component.html',
-  styleUrls: ['./customer-one.component.css']
+  styleUrls: ['./customer-one.component.css'],
 })
-export class CustomerOneComponent {
+export class CustomerOneComponent implements OnInit {
+  // customerForm is a property that has FormGroup as its type and this type defines our form model
+  customerForm!: FormGroup;
+  // customer is a data model and its passed to and from a back-end server
+  customer = new Customer();
 
+  constructor() {}
+
+  ngOnInit() {
+    // the code below creates an instance of the FormGroup and assigns it to the customerForm property
+    this.customerForm = new FormGroup({
+      // we add a FormControl for each input element in the template's form
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      // the default value for sendCatalog is true
+      sendCatalog: new FormControl(true),
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
 }
