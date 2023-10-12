c7253132729c678fe8fdba1eb6c0095998580c98
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-12 23:51:05 +0100

    fixed the object returning null bug i was having

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CustomerOneComponent } from './customers/customer-one.component';
 
 @NgModule({
   declarations: [AppComponent, CustomerComponent, CustomerOneComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
